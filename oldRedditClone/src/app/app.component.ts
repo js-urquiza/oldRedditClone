7a6f7f82dadb4b70b497f6d7b4ec5c96b5c812a0
juansantiago <js.urquiza@gmail.com>
2023-01-20 16:59:52 -0300

    [app.component] addArticle method was modified in order to push a new article from the form

--- oldRedditClone/src/app/app.component.ts
+++ oldRedditClone/src/app/app.component.ts
@@ -4,10 +4,9 @@ import { Article } from './article/article.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
   //Añado una nueva propiedad que es un array de artículos
   articles: Article[];
 
@@ -16,12 +15,19 @@ export class AppComponent {
       new Article('Angular', 'http://angular.io', 3),
       new Article('Fullstack', 'http://fullstack.io', 2),
       new Article('Github', 'http://github.com', 1),
-    ]
+    ];
   }
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    //Agrega un nuevo artículo
+    this.articles.push(new Article(title.value, link.value, 0));
+    //Pushea un nuevo artículo con: 1) título 2) link y 3) el puntaje que, como recién empieza, le pondremos 0.
+    
+    //Limpiar el input del formulario
+    title.value = '';
+    link.value = '';
+
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
     return false;
   }
-
 }
